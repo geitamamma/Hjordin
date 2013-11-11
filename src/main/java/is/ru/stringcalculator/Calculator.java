c2ba8fa3b6710fd56d304fd09ab0f5d13b35022e
hannesg11 <hannesg11@ru.is>
2013-11-11 14:44:01 +0000

    Breyta skram

---
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -0,0 +1,7 @@
+package geitamamma.hjordin.stringcalculator
+
+public class Calculator{
+	public static int add(String text){
+		return 0;
+	}
+}
---
+++ test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -0,0 +1,12 @@
+package geitamamma.hjordin.stringcalculator
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest{
+
+	@Test
+	public void testEmptyString(){
+		assertEquals(0, Calculator.add(""));
+	}
+}
